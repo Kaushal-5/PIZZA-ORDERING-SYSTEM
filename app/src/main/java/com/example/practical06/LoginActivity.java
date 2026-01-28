41963409c811aab4c321f30b4a9839af5cb53a5e
kaushal-commits <kaushal@sasahyog.com>
2026-01-28 12:28:49 +0530

    Code: Add Log import to LoginActivity

--- app/src/main/java/com/example/practical06/LoginActivity.java
+++ app/src/main/java/com/example/practical06/LoginActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
